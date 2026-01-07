7f71e7fd0e86b449e6269f071dbc23a1dfe85e0e
vpistori.lunaro <vpistori.lunaro@ion.com.mx>
2026-01-07 18:05:41 -0300

    feature/ include pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "bookstack-ai",
+        short_name: "bookstack",
+        description:
+          "Gerencie sua biblioteca pessoal com inteligência artificial",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
+      "/api": {
+        target: "http://127.0.0.1:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
-      '/uploads': {
-        target: 'http://127.0.0.1:8000',
+      "/uploads": {
+        target: "http://127.0.0.1:8000",
         changeOrigin: true,
       },
     },
   },
-})
+});
