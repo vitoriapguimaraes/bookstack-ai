d59cda46dadc94d01990320948651e59d1225ddb
vpistori.lunaro <vpistori.lunaro@ion.com.mx>
2026-01-10 10:42:30 -0300

    fix/ auth

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -46,3 +46,6 @@ export const DEFAULT_AVAILABILITY_OPTIONS = [
   "Emprestado",
   "N/A",
 ];
+
+export const SUPER_ADMIN_EMAIL = "vipistori@gmail.com";
+export const CONTACT_EMAIL = "bookstackai@gmail.com";
